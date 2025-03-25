50280c78a146c16b762bb70dec9d5f1071534af4
AleksandraFilonenko <aleksa_filonenko@mail.ru>
2025-03-25 19:44:06 +0300

    feat: calc

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -4,24 +4,21 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        Scanner scannerString = new Scanner(System.in);
+        String input = scannerString.nextLine();
 
-     Scanner scannerString = new Scanner(System.in);
-     String input = scannerString.nextLine();
-
-            try {
-                String calc = calc(input);
-                System.out.println(calc);
-            } catch (Exception e) {
-                System.out.println( e.getMessage());
-
-            }
-
+        try {
+            String calc = calc(input);
+            System.out.println(calc);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+    }
 
     public static String calc(String input) throws Exception {
         input = input.replaceAll("\\s+", "");
 
-        String [] parts = input.split("[+\\-*/]");
+        String[] parts = input.split("[+\\-*/]");
 
         if (parts.length != 2) {
             throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
@@ -29,21 +26,20 @@ public class Calculator {
 
         int firstNumber = Integer.parseInt(parts[0]);
         int secondNumber = Integer.parseInt(parts[1]);
-        if (firstNumber < 1 || firstNumber > 10 || secondNumber < 1 || secondNumber > 10){
+        if (firstNumber < 1 || firstNumber > 10 || secondNumber < 1 || secondNumber > 10) {
             throw new Exception("Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более.");
         }
 
-        if (input.contains("+")){
+        if (input.contains("+")) {
             return String.valueOf(firstNumber + secondNumber);
-        }else  if (input.contains("-")) {
+        } else if (input.contains("-")) {
             return String.valueOf(firstNumber - secondNumber);
-        }else if (input.contains("*")){
+        } else if (input.contains("*")) {
             return String.valueOf(firstNumber * secondNumber);
-        } else if (input.contains("/")){
+        } else if (input.contains("/")) {
             return String.valueOf(firstNumber / secondNumber);
-        }else {
+        } else {
             throw new Exception("строка не является математической операцией");
         }
-
     }
 }
